43a1edce2c8989774c08cda69ae9e1c9da9e3866
dchamindu826 <dchamindu826@gmail.com>
2026-01-08 18:52:59 +0530

    updatess

--- src/config.js
+++ src/config.js
@@ -1,4 +1,3 @@
-// Localhost එකේ වැඩ කරනකොට මේක දාන්න:
-export const API_BASE_URL = "http://localhost:5000"; 
+//export const API_BASE_URL = "http://localhost:5000"; 
 
-// Vercel Deploy කරනකොට විතරක් අරක දාන්න (https://chat-bot-system-two.vercel.app)
+export const API_BASE_URL = "https://chat-bot-system-two.vercel.app"; 
