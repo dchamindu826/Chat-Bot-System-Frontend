b1eb40e9db8c51a4e0dbc65882c9ecc32e3770d6
dchamindu826 <dchamindu826@gmail.com>
2026-01-09 11:54:25 +0530

    config

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const API_BASE_URL = "http://localhost:5000"; 
+//export const API_BASE_URL = "http://localhost:5000"; 
 
-//export const API_BASE_URL = "https://chat-bot-system-two.vercel.app"; 
+export const API_BASE_URL = "https://chat-bot-system-two.vercel.app"; 
